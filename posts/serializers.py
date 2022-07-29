79cc1b21314dd1b83101d8e9c06debdd783ffeff
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2022-07-29 08:16:19 +0300

    Force current user as post author

--- posts/serializers.py
+++ posts/serializers.py
@@ -3,6 +3,19 @@ from posts.models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    '''
+    Read-only fields are included in the API output but should not be included
+    in the input during create or update operations. Any 'read_only' fields
+    that are incorrectly included in the serializer input will be ignored.
+
+    Set this to True to ensure that the field is used when serializing
+    a representation, but is not used when creating or updating an instance
+    during deserialization.
+    '''
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+    title = serializers.CharField(max_length=128)
+    body = serializers.CharField(max_length=512)
+
     class Meta:
         model = Post
-        fields = "__all__"
+        fields = ("user", "title", "body")
